38a3fd7241c8feb4f4a68e45507208d300e2e10f
ほげ <hogehoge@example.com>
2017-06-08 17:32:09 +0900

    hotfix

--- make.js
+++ make.js
@@ -1,18 +1,18 @@
 var fs = require('fs');
 
-// kuromoji 設定
+//kuromoji 設定
 var kuromoji = require('kuromoji');
 var builder = kuromoji.builder({
   dicPath: 'node_modules/kuromoji/dict'
 });
 
-// マルコフ連鎖
+//マルコフ連鎖
 class Markov {
   constructor(n) {
     this.data = {};
   }
 
-  // データ登録
+  //データ登録
   add(words) {
     for(var i = 0; i <= words.length; i++) {
       var now = words[i];
@@ -27,7 +27,7 @@ class Markov {
     }
   }
 
-  // 指定された文字に続く文字をランダムに返す
+  //指定された文字に続く文字をランダムに返す
   sample(word) {
     var words = this.data[word];
     if(words === undefined) { words = []; }
@@ -35,7 +35,7 @@ class Markov {
     return words[Math.floor(Math.random() * words.length)];
   }
 
-  // マルコフ連鎖でつなげた文を返す
+  //マルコフ連鎖でつなげた文を返す
   make() {
     var sentence = [];
     var word = this.sample(null);
@@ -52,24 +52,24 @@ var markov = new Markov();
 builder.build(function(err, tokenizer) {
   if(err) { throw err; }
 
-  // txtを読み込む
+  //txtを読み込む
   fs.readFile('list.txt', 'utf8', function(err, data) {
     if(err) { throw err; }
 
-    var lines = data.split("\n"); // 一行ごとに分割
+    var lines = data.split("\n"); //一行ごとに分割
     lines.forEach(function(line) {
       var tokens = tokenizer.tokenize(line);
 
-      // トークンを文中表記に変換
+      //トークンを文中表記に変換
       var words = tokens.map(function(token) {
         return token.surface_form;
       });
 
-      // データ登録
+      //データ登録
       markov.add(words);
     });
 
-    // 10回生成
+    //10回生成
     for(var n = 0; n < 10; n++) {
       console.log(markov.make());
     }
