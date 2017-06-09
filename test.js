ed7300746923563a888338d08b71b13fd4210a4e
ほげ <hogehoge@example.com>
2017-06-09 14:27:51 +0900

    hotfix

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var kuromoji = require('kuromoji');
+var fs = require('fs');
 
 // builder = 形態素解析用オブジェクト
 var builder = kuromoji.builder({
@@ -12,6 +13,12 @@ builder.build(function(err, tokenizer) {
   if(err) { throw err; }
 
   // tokenizer.tokenize に文字列を渡しその文を形態素解析
-  var tokens = tokenizer.tokenize("どうしてそんなひどいこと言うの？");
+  var tokens = tokenizer.tokenize("うちの猫ねこが、傷きずだらけになって帰かえってきた。他ほかの猫ねことけんかをしたみたいだ。");
   console.dir(tokens);
+  // txtに出力
+  var writeTokens = JSON.stringify(tokens, null , "\t"); 
+  fs.writeFile('output.txt', writeTokens , function (err) {
+      console.log(err);
+  });
 });
+
